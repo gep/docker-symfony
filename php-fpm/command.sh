f570e0a27e22595334498ffd6cae39990c1c1fc9
Andrey Semikov <andrew.semikov@gmail.com>
2016-08-12 12:49:06 +0300

    Correct docker compose

--- php-fpm/command.sh
+++ php-fpm/command.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+cd /var/projects/symfony/app
+
+mkdir -p /var/projects/symfony/app/vendor
+
+composer install --no-interaction --no-suggest
+
 chmod -R 777 /var/projects/symfony/app/var/cache
 chmod -R 777 /var/projects/symfony/app/var/logs
+chmod -R 777 /var/projects/symfony/app/var/sessions
+
 php-fpm
